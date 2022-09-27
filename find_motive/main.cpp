fa5f6122a46b943de1382a79b216179eaa05ff81
capinho <papadiawara@hotmail.fr>
2022-09-27 23:58:52 +0200

    add review of group 35

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -10,35 +10,36 @@
 #include <map>
 #include <string>
 
-using namespace std;
-typedef map<string, int> StrIntMap;
+typedef std::map<std::string, int> StrIntMap;
 
-void find_motive(istream& in, StrIntMap& words ,string w) {
-    string s;
+void find_motive(std::istream& in, StrIntMap& words ,std::string w) {
+    std::string s;
     while (in >> s) {
         // Find position of string and Check if position is -1 or not
-        if(s.find(w)!= string::npos){
+        if(s.find(w)!= std::string::npos){
                 ++words[s];
         }
     }
 }
+
 
 int main(int argc, char** argv) {
 
     if (argc < 3)
         return(EXIT_FAILURE);
     
-    ifstream in(argv[1]);
+    std::ifstream in(argv[1]);
     
-    if (!in.is_open())
-        cout << "The file *" << argv[1] << "* could not be opened." << endl;
+    if (!in.is_open()){
+        std::cout << "The file *" << argv[1] << "* could not be opened.";
+        return 1;
+
+    }
     else{
-        string motive = argv[2];
+        std::string motive = argv[2];
         StrIntMap wo;
-        find_motive(in, wo, motive);
-        cout << motive << " occurred "
-            << wo.size() << " times.\n";
-    
+        find_motive(in,wo, motive);
+        std::cout << " The file " << argv[1] << "contains " << wo.size() << " words containing the motive " << motive;
     }
 
     return 0;
