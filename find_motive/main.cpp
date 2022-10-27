15b8dcddbeb4705f55df94334b3533fb3a5827b1
Gregory L <gregoryslaurent@gmail.com>
2022-10-27 11:13:54 +0200

    corrected

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -7,39 +7,38 @@
 
 #include <iostream>
 #include <fstream>
-#include <map>
 #include <string>
+#include <regex>
 
+using namespace std;
 
-unsigned find_motive(std::istream& in,std::string w) {
-    std::string s;
-    int count=0;
-    while (in >> s) {
-        // Find position of string and Check if position is -1 or not
-        if(s.find(w)!= std::string::npos){
-            count++;
+int main(int argc, char* argv[])
+{
+    string path = argv[1];
+    string pattern = argv[2];
+    string line;
+    string word;
+
+    ifstream myfile(path);
+
+    int word_count = 0;
+
+    if (myfile.is_open())
+    {
+        while (myfile >> word)
+        {
+            if (word.find(pattern) != std::string::npos)
+            {
+                ++word_count;
+            }
         }
+        myfile.close();
+        cout << "The file " << path << " contains " << word_count << " words containing the motive " << pattern << "\n";
     }
-    return count;
-}
-
-
-int main(int argc, char** argv) {
-
-    if (argc < 3)
-        return(EXIT_FAILURE);
-
-    std::ifstream in(argv[1]);
-
-    if (!in.is_open()){
-        std::cout << "The file *" << argv[1] << "* could not be opened.";
+    else
+    {
+        cout << "The file " << path << " could not be opened.\n";
         return 1;
-
     }
-    else{
-        std::string motive = argv[2];
-        std::cout << " The file " << argv[1] << " contains " << find_motive(in, motive) << " words containing the motive " << motive;
-    }
-    
     return 0;
 }
--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -1,14 +1,13 @@
-/// pape ibrahima diawara
 #include <iostream>
 
 bool est_premier(int p)
 {
-    
+
     if (p <= 1) {
         return false;
     }
 
-    for (int i = 2; i <= p/2; ++i) {
+    for (int i = 2; i <= p / 2; ++i) {
         if (p % i == 0) {
             return false;
         }
@@ -16,15 +15,16 @@ bool est_premier(int p)
     return true;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int n;
-    while(std::cin >> n)
+    while (std::cin >> n)
     {
-        if(est_premier(n))
+        if (est_premier(n))
         {
             std::cout << n << " is a prime: True\n";
-        }else
+        }
+        else
             std::cout << n << " is a prime: False\n";
     }
     return 0;
